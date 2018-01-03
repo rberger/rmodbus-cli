109ccd86794f360562cf7715e95cbc902c4dfd3d
Robert J. Berger <rberger@ibd.com>
2018-01-03 00:34:29 -0800

    Made attributes out of CLI opts, Read in BOD.json and make it a clean dictionary.

--- lib/rmodbus_cli/cli.rb
+++ lib/rmodbus_cli/cli.rb
@@ -2,11 +2,11 @@ require "slop"
 
 module RmodbusCli
   class Cli
-
     def doit
       opts = Slop.parse do |o|
         o.string '-t', '--tty', 'Serial (USB) Port', default: '/dev/ttyUSB0'
         o.integer '-b', 'baudrate', 'Serial port baudrate', default: 115200
+        o.string '-d', '--dictionary_file', 'Path to json file that contains Grin Modbus Dictionary', default: "BOD.json"
         o.integer '-s', 'slave_id', "Modbus slave ID", default: 1
         o.integer '-a', 'address', "Slave Register Starting Address", default: 312
         o.integer '-c', 'count', "Number of Slave Register Addresses", default: 1
@@ -21,8 +21,8 @@ module RmodbusCli
       end
 
       puts opts.to_hash
-      modbus = RmodbusCli::Modbus.new
-      modbus.read_value(opts)
+      modbus = RmodbusCli::Modbus.new(opts.to_hash)
+      modbus.read_value(opts[:address], opts[:count])
     end
   end
 end
--- lib/rmodbus_cli/modbus.rb
+++ lib/rmodbus_cli/modbus.rb
@@ -1,11 +1,54 @@
 require "rmodbus"
+require "json"
 
 module RmodbusCli
   class Modbus
-    def read_value(opts)
-      cl = ::ModBus::RTUClient.new(opts[:tty], opts[:baudrate])
+    # Contains the Grin Phaesrunner Modbus Dictionary
+    # @params [Hash<Integer, Hash>] dict The Dictionary with a key for each register address
+    # @option dict [String] :name Name of the register
+    # @option dict [Integer] :address Address of register
+    # @option dict [Integer] :accessLevel Access Level of register
+    # @option dict [Boolean] :read If the register can be read
+    # @option dict [Boolean] :write If the register can be written
+    # @option dict [Boolean] :saved If the register has been saved
+    # @option dict [Integer,Float,String] :scale How to scale the raw value
+    # @option dict [String] :units The units for the value
+    # @option dict [Stirng] :type Further info on how to interpret the value
+    attr_reader :dict
+
+    # New Modbus
+    #  Converts the opts hash into Class Instance Variables (attr_readers)
+    #  Reads the JSON Grin Phaserunner Modbus Dictionary into a Hash
+    # @params opts [Hash] comes from the CLI
+    def initialize(opts)
+      # Converts each key of the opts hash from the CLI into individual class attr_readers.
+      # So they are now available to the rest of this Class as instance variables.
+      # The key of the hash becomes the name of the instance variable.
+      # They are available to all the methods of this class
+      # See https://stackoverflow.com/a/7527916/38841
+      opts.each_pair do |name, value|
+        self.class.send(:attr_accessor, name)
+        instance_variable_set("@#{name}", value)
+      end
+
+      @dict = clean_dict(JSON.parse(File.read(dictionary_file)))
+    end
+
+    # Make the Dictionary an pleasant hash with Integer top keys (addresses) and
+    # symbols for other keys
+    # @params [Hash<String, Hash>] original_dict The Dictionary as it came from JSON
+    def clean_dict(original_dict)
+      Hash[original_dict.map do |address, hash|
+             [address.to_i, Hash[hash.map { |k,v| [k.to_sym, v] }]]
+           end
+          ]
+    end
+
+    def read_value(start_address, count)
+      puts("Reading from #{dict[start_address][:name]}")
+      cl = ::ModBus::RTUClient.new(tty, baudrate)
       cl.with_slave(opts[:slave_id]) do |slave|
-        puts slave.read_holding_registers(opts[:address], opts[:count])
+        puts slave.read_holding_registers(start_address, count)
       end
     end
   end
